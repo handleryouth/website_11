89e89e12192e521cfbbae4e4beaf6749f3482c4d
handleryouth <70279376+handleryouth@users.noreply.github.com>
2021-03-08 21:36:27 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
+answer = document.querySelectorAll(".answer")
+question = document.querySelectorAll(".question")
 
-document.getElementById("first").addEventListener("click", function(){
-  if(document.querySelectorAll(".answer")[0].style.visibility === "hidden"){
-    document.querySelectorAll(".answer")[0].style.visibility = "visible"
-    document.querySelectorAll(".arrow-down")[0].style.transform = "rotate(180deg)"
-  }
-  else{
-    document.querySelectorAll(".answer")[0].style.visibility = "hidden"
-    document.querySelectorAll(".arrow-down")[0].style.transform = "rotate(0deg)"
-  }
-})
+  question[0].addEventListener("click", function() {
+    if (answer[0].classList[1] === "hidden") {
+      answer[0].classList.remove("hidden");
+      answer[0].classList.add("visible");
+      document.querySelectorAll(".arrow-down")[0].style.transform = "rotate(180deg)";
+    } else {
+      answer[0].classList.remove("visible");
+      answer[0].classList.add("hidden");
+      document.querySelectorAll(".arrow-down")[0].style.transform = "rotate(0deg)";
+    }
+  })
 
-document.getElementById("second").addEventListener("click", function(){
-  if(document.querySelectorAll(".answer")[1].style.visibility === "hidden"){
-    document.querySelectorAll(".answer")[1].style.visibility = "visible"
-    document.querySelectorAll(".arrow-down")[1].style.transform = "rotate(180deg)"
-  }
-  else{
-    document.querySelectorAll(".answer")[1].style.visibility = "hidden"
-    document.querySelectorAll(".arrow-down")[1].style.transform = "rotate(0deg)"
-  }
-})
+  question[1].addEventListener("click", function() {
+    if (answer[1].classList[1] === "hidden") {
+      answer[1].classList.remove("hidden");
+      answer[1].classList.add("visible");
+      document.querySelectorAll(".arrow-down")[1].style.transform = "rotate(180deg)";
+    } else {
+      answer[1].classList.remove("visible");
+      answer[1].classList.add("hidden");
+      document.querySelectorAll(".arrow-down")[1].style.transform = "rotate(0deg)";
+    }
+  })
 
-document.getElementById("third").addEventListener("click", function(){
-  if(document.querySelectorAll(".answer")[2].style.visibility === "hidden"){
-    document.querySelectorAll(".answer")[2].style.visibility = "visible"
-    document.querySelectorAll(".arrow-down")[2].style.transform = "rotate(180deg)"
-  }
-  else{
-    document.querySelectorAll(".answer")[2].style.visibility = "hidden"
-    document.querySelectorAll(".arrow-down")[2].style.transform = "rotate(0deg)"
-  }
-})
+  question[2].addEventListener("click", function() {
+    if (answer[2].classList[1] === "hidden") {
+      answer[2].classList.remove("hidden");
+      answer[2].classList.add("visible");
+      document.querySelectorAll(".arrow-down")[2].style.transform = "rotate(180deg)";
+    } else {
+      answer[2].classList.remove("visible");
+      answer[2].classList.add("hidden");
+      document.querySelectorAll(".arrow-down")[2].style.transform = "rotate(0deg)";
+    }
+  })
 
-document.getElementById("fourth").addEventListener("click", function(){
-  if(document.querySelectorAll(".answer")[3].style.visibility === "hidden"){
-    document.querySelectorAll(".answer")[3].style.visibility = "visible"
-    document.querySelectorAll(".arrow-down")[3].style.transform = "rotate(180deg)"
-  }
-  else{
-    document.querySelectorAll(".answer")[3].style.visibility = "hidden"
-    document.querySelectorAll(".arrow-down")[3].style.transform = "rotate(0deg)"
-  }
-})
+  question[3].addEventListener("click", function() {
+    if (answer[3].classList[1] === "hidden") {
+      answer[3].classList.remove("hidden");
+      answer[3].classList.add("visible");
+      document.querySelectorAll(".arrow-down")[3].style.transform = "rotate(180deg)";
+    } else {
+      answer[3].classList.remove("visible");
+      answer[3].classList.add("hidden");
+      document.querySelectorAll(".arrow-down")[3].style.transform = "rotate(0deg)";
+    }
+  })
 
-document.getElementById("fifth").addEventListener("click", function(){
-  if(document.querySelectorAll(".answer")[4].style.visibility === "hidden"){
-    document.querySelectorAll(".answer")[4].style.visibility = "visible"
-    document.querySelectorAll(".arrow-down")[4].style.transform = "rotate(180deg)"
-  }
-  else{
-    document.querySelectorAll(".answer")[4].style.visibility = "hidden"
-    document.querySelectorAll(".arrow-down")[4].style.transform = "rotate(0deg)"
-  }
-})
+  question[4].addEventListener("click", function() {
+    if (answer[4].classList[1] === "hidden") {
+      answer[4].classList.remove("hidden");
+      answer[4].classList.add("visible");
+      document.querySelectorAll(".arrow-down")[4].style.transform = "rotate(180deg)";
+    } else {
+      answer[4].classList.remove("visible");
+      answer[4].classList.add("hidden");
+      document.querySelectorAll(".arrow-down")[4].style.transform = "rotate(0deg)";
+    }
+  })
